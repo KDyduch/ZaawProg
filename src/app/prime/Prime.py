c28e81c36ed5d9df99fc0e4ac5f0a5c64107126d
KDyduch <118347980+KDyduch@users.noreply.github.com>
2023-01-20 18:15:12 +0100

    Update Prime.py

--- src/app/prime/Prime.py
+++ src/app/prime/Prime.py
@@ -1,14 +1,24 @@
-from fastapi import APIRouter
-import sympy
+from fastapi import FastAPI
+import math
+import uvicorn
 
-prime_router = APIRouter()
+app = FastAPI()
 
-
-@prime_router.get("/prime/{number}")
-async def prime(number: int):
-    isPrime = sympy.isprime(number)
-
-    return {
-        "is_prime": isPrime,
-        "number": number,
+@app.get('/prime/{number}')
+def prime(number: int):
+    response = {
+        "Number": number,
+        "Prime": is_prime(number)
     }
+    return response
+
+def is_prime(number: int):
+    if(number <= 1):
+        return False
+    for i in range(2, int(math.sqrt(number))+1):
+        if(number%i == 0):
+            return False
+    return True
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=5000)
